8bc486b7db04d56ce90aa1cf5e03dbcb608a1ec3
MrCr <oak7887@gmail.com>
2023-03-23 11:07:30 +0000

    Lab2 Reclusive

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include "mbed.h"
+
+int nth_term=14;
+
+int fib(int n);
+
+int main() 
+
+{
+ printf("Lab 2 Program \r\n");      
+for(int i=0; i<nth_term;i++)
+
+        {
+        printf("%d, ", fib(i));
+        }
+
+printf("..!\r\n");      
+}
+
+int fib(int n)
+
+{
+if (n==0) return 0;
+if (n==1) return 1;
+else
+
+    {
+    return (fib(n-1)+fib(n-2));
+    }
+
+}
